95b8fc5bf1d926eda9d21e108acf0cf398ef0f67
Reinier Melian Massip <reinier.melian@isofact.com>
2017-06-07 08:10:44 -0400

    Initial Commit

---
+++ ImageAspectFromImagePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageAspectFromImagePicker
+//
+//  Created by Reinier Melian on 07/06/2017.
+//  Copyright © 2017 Reinier Melian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageAspectFromImagePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageAspectFromImagePicker
+//
+//  Created by Reinier Melian on 07/06/2017.
+//  Copyright © 2017 Reinier Melian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
